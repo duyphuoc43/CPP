e996944bfd2bd8f98a34798a65db1bf123358c84
Duy Phước <135586447+duyphuoc43@users.noreply.github.com>
2023-11-09 14:34:01 +0700

    Add files via upload

--- process.cpp
+++ process.cpp
@@ -5,22 +5,23 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
     ofstream file ("share_file.txt");
     file << "Sever turn on" << std::endl;
     file.close();
 
     pid_t pid = fork();
-
-    if (pid == -1) 
+    bool check_client = false;
+    bool check_sever = true;
+    while (true)
     {
-        cout<<(stderr, "Fork failed\n");
-        return 1;
-    } 
-    else if (pid == 0) 
-    {
-        while (true)
+        if (pid == -1)
+        {
+            cout<<(stderr, "Fork failed\n");
+            return 1;
+        }
+        else if (pid == 0 && check_client)
         {
             string text,newtext,reptext;
             ifstream readfile ("share_file.txt");
@@ -35,12 +36,20 @@ int main()
             file <<reptext<<endl;
             file.close();
             cout<<"wait sever reply"<<endl;
-            sleep(3);
+            int status;
+            if (waitpid(pid, &status, WNOHANG) != 0)
+            {
+                check_sever = true;
+                check_client = false;
+            }
+            // else
+            // {
+            //     sleep(3);
+            // }
+
+
         }
-    } 
-    else 
-    {
-        while (true)
+        else if (pid > 0 && check_sever)
         {
             string text,newtext,reptext;
             ifstream readfile ("share_file.txt");
@@ -55,9 +64,21 @@ int main()
             file << reptext <<endl;
             file.close();
             cout<<"wait client reply"<<endl;
-            sleep(3);
+        
+            int status;
+            if (waitpid(pid, &status, WNOHANG) != 0) 
+            {
+                check_client = true;
+                check_sever = false;
+            }
+            // else
+            // {
+            //     sleep(3);
+            // }
+
+  
         }
     }
 
-    return 0;
+return 0;
 }
